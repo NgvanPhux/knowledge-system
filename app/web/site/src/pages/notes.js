de5f5638ed96dfb451b1d92ab2ac2bb96c3b8364
NgvanPhux <vanphuc09035@gmail.com>
2026-05-13 11:46:52 +0700

    fix vercel linux case sensitive paths

--- app/web/site/src/pages/notes.js
+++ app/web/site/src/pages/notes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "@theme/Layout";
-import ModuleNote from "../components/Modulenote/modulenote";
+import ModuleNote from "../components/ModuleNote/ModuleNote";
 
 export default function NotesPage() {
   return (
